301ef283aeaa9c2c49593335ce1906b8ada70168
theakite <aaronkite@live.com>
2017-07-03 20:17:00 -0500

    Successful C++ reading of XML file

--- Source/test.cpp
+++ Source/test.cpp
@@ -1,5 +1,8 @@
 #include "../Libraries/rapidxml-1.13/rapidxml.hpp"
+#include "../Libraries/rapidxml-1.13/rapidxml_utils.hpp"
+
 #include <iostream>
+#include <cstring>
 
 using namespace rapidxml;
 using namespace std;
@@ -7,7 +10,27 @@ using namespace std;
 
 int main() {
 
-    cout<<"It does a thing!\n";
+    file<> xmlFile("test.xml"); //TODO: needs to have test.xml as a variable in a config file
+    clog << "XML Loaded" << endl;
 
+    xml_document<> doc;
+    doc.parse<0>(xmlFile.data());
+    clog << "XML Parsed" << endl;
+
+    xml_node<> *topNode=doc.first_node(0);
+
+    //TODO: have something for version checking
+    
+    if (!strcmp(topNode->name(), "parameterFile")) { //TODO: needs to have "parameterFile" as var in config
+        clog << "Top Node Loaded at " << topNode << endl;
+    }
+
+    else {
+        clog << "Unexpected first node: " << topNode->name() <<  endl;
+        cerr << "Unexpected first node: " << topNode->name() <<  endl;
+
+        return 0;
+    }
+    
     return 0;
 }
