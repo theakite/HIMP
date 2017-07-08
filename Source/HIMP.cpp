4978fd820585e8efc8f7ece49bffa6b4c488cade
theakite <aaronkite@live.com>
2017-07-08 16:12:07 -0500

    Migration to new name and children counting

--- Source/HIMP.cpp
+++ Source/HIMP.cpp
@@ -7,6 +7,7 @@
 using namespace rapidxml;
 using namespace std;
 
+int countChildren(xml_node<> parentNode);
 
 int main() {
 
@@ -18,33 +19,28 @@ int main() {
     clog << "XML Parsed" << endl;
 
     xml_node<> *topNode=doc.first_node(0);
+    int topChildrenCount = count_children(topNode);
 
     //TODO: have something for version checking
     
     if (!strcmp(topNode->name(), "parameterFile")) { //TODO: needs to have "parameterFile" as var in config
-        clog << "Top Node Loaded at " << topNode << endl;
+        clog << "Top Node Loaded at " << topNode << " and has " << topChildrenCount << " children" << endl;
     }
 
     else {
-        clog << "Unexpected first node: " << topNode->name() <<  endl;
-        cerr << "Unexpected first node: " << topNode->name() <<  endl;
+        clog << "Unexpected top node: " << topNode->name() <<  endl;
+        cerr << "Unexpected top node: " << topNode->name() <<  endl;
 
-        return 0;
+        return 0; //TODO: handle this quit more gracefully
     }
 
-    //TODO: cycle thorough next-generation nodes and store in an array
-    xml_node<> *firstChild = topNode->first_node(0);
-    if (firstChild != 0) { //There's a node
-        //count the nodes
-        cout << "Child Node Found: " << firstChild << endl;
+    xml_node<> topChildren[topChildrenCount] = new xml_node<>[];
+    topChildren[0] = topNode->first_node(0);
+    for (int i = 1; i < topChildrenCount; i++) {
+        topChildren[i] = topChildren[i-1].next_sibling(0);
     }
 
-    else { //There isn't a child node. This is unexpected
-        clog << "No Child Node Found" << endl;
-        cerr << "No Child Node Found" << endl;
-    }
-
-
+    cout << "we did it" << endl;
 
     return 0;
 }
